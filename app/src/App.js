fa36758a34142c7234442390c5e1c31ceec8d04d
Joe Shellard <joeshellard@gmail.com>
2021-07-01 17:38:28 +0100

    Basic Welcome panel

--- app/src/App.js
+++ app/src/App.js
@@ -11,6 +11,7 @@ import { useContract } from "./useContract";
 import { default as GameFactoryContract } from "./contracts/GameFactory.json";
 import { config } from "./config";
 import { useContractFn } from "./useContractFn";
+import { OrnateBorder } from "./OrnateBorder";
 import {
   BrowserRouter as Router,
   Switch,
@@ -139,9 +140,9 @@ function App() {
         <div className="background-gradient"></div>
         <div className="intro">
           <Link to="/">
-            <h1 className="logo">
+            <h2 className="logo">
               Ledgernomi<span style={{ fontVariant: "small-caps" }}>x</span>
-            </h1>
+            </h2>
           </Link>
           <div className="links">
             <h3>How to Play</h3>
@@ -150,6 +151,33 @@ function App() {
         </div>
         <div className="all-panels-container">
           <div className="background-spacer"></div>
+          <div className="welcome panel">
+            <div className="background-pattern"></div>
+            <OrnateBorder></OrnateBorder>
+            <h1 className="logo">
+              Ledgernomi<span style={{ fontVariant: "small-caps" }}>x</span>
+            </h1>
+            <h3>the game of the rules of the game</h3>
+            <br></br>
+            <div>
+              Ledgernomix is a game of political economy, played on a
+              blockchain, with real cryptocurrency.
+            </div>
+            <br></br>
+            <div>
+              Play with your friends in real time, like a board game, or over a
+              longer time, like correspondence chess.
+            </div>
+            <br></br>
+            <div>
+              Each game of Ledgernomix is a distributed autonomous organisation,
+              or DAO, governed by a contract that exists on the blockchain. You
+              can also think of it as a self-contained model economy and model
+              parliament, with you and your friends as the members.
+            </div>
+            <br></br>
+            <div></div>
+          </div>
           <Setup setupStatus={setupStatus}></Setup>
           <Switch>
             <Route path="/:gameAddress">
