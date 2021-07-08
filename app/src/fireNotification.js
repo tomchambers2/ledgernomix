f2c913913004a7495c02d48bdb13c9027c9bc70e
Tom Chambers <tom.chambers@gmail.com>
2021-07-08 17:59:17 +0100

    style noti

--- app/src/fireNotification.js
+++ app/src/fireNotification.js
@@ -1,9 +1,9 @@
 import "react-toastify/dist/ReactToastify.css";
 import { toast } from "react-toastify";
 
-export const fireNotification = function (text, type) {
+export const fireNotification = function (text) {
   toast(text, {
-    style: { fontSize: "200%", color: "white", backgroundColor: "red" },
+    style: { fontSize: "200%", color: "black" },
     position: "top-center",
   });
 };
