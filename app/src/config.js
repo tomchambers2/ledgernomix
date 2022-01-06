d73b85a257ef6cdb7dde9323bc827917893f4173
Tom Chambers <tom.chambers@gmail.com>
2022-01-06 15:35:56 +0000

    deployed to mainnet v0.1 0x521AdbA73Cc99FAcf5B52721D426c95e84F80Fbc

--- app/src/config.js
+++ app/src/config.js
@@ -1,1 +1,1 @@
-export const config = { gameFactoryContract: { address: "0xda8EA8aB4729514Dd0Bb9ff383350F3A750a3Ff7" } };
+export const config = { gameFactoryContract: { address: "0x521AdbA73Cc99FAcf5B52721D426c95e84F80Fbc" } };
