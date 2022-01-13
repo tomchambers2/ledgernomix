f260ca25ff0d27386a4044615add9a0e1534f1b8
Joe Shellard <joeshellard@gmail.com>
2022-01-13 18:28:08 +0000

    Updated how to play

--- app/src/HowToPlay.js
+++ app/src/HowToPlay.js
@@ -23,19 +23,24 @@ export const HowToPlay = () => {
         laugh off such a loss.
       </div>
       <div className="paragraph">
-        If you're playing on our test network, the currency isn't worth anything
-        anyway, but we can pretend! To get some currency on our network, you'll
-        need to copy your address from Metamask, and send it to someone who
-        already has currency on the network, who can make a transfer to you.
-        We'd recommend a balance of around 6.00 to play - 5.00 for the game fee
-        and 1.00 to be sure of covering all the transaction fees you're likely
-        to incur as you play.
+        To get some xDAI, you could copy your address, which can be found under
+        the account name near the top of the Metamask window, and send it to
+        someone who already has some xDAI, who can make a transfer to you.
+        Alternatively, you can buy xDAI using Ramp or Mt Pellerin. We'd
+        recommend a balance of around 6.00 to play - 5.00 for the game pot and
+        1.00 to be sure of covering all the transaction fees you're likely to
+        incur as you play.
       </div>
       <div className="paragraph">
         One player needs to start the game. This will generate a game name and a
         URL. You can invite other players by sending them the URL or having them
         click on the game name in the list of games accessible from the main
-        page.
+        page. Players join by clicking the round Join Game button at the top of
+        the game page. To keep out any uninvited guests, prospective players
+        have to be admitted by the existing players, who can do this by clicking
+        on the Admit buttons in the proposals panel, which show each prospective
+        player's address. Players should confirm their addresses. Check that you
+        have the number of players you expect before you start playing.
       </div>
       <div className="paragraph">
         You may find that you need to refresh a few times, close and re-open
@@ -48,12 +53,10 @@ export const HowToPlay = () => {
         In that case, the transaction will be rejected - that's normal too.
       </div>
       <div className="paragraph">
-        Make sure all players have joined (by clicking the round Join Game
-        button) and that you have the number of players you expect before you
-        start playing. Your player name (eg. Player A, B, C) is displayed in the
-        panel on the top right. The Proposals panel in the lower centre will
-        show whose turn it is to start or, if it's you, you'll have an input
-        panel allowing you to select different rules to propose changes to.
+        Your player name (eg. Player A, B, C) is displayed in the panel on the
+        top right. The Proposals panel in the lower centre will show whose turn
+        it is to start or, if it's you, you'll have an input panel allowing you
+        to select different rules to propose changes to.
       </div>
       <div className="paragraph">
         Most of the panels you see in the game have extra information available
