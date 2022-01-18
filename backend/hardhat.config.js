0618f341a4edc1141475be434620e9cabdc1cb3a
Tom Chambers <tom.chambers@gmail.com>
2022-01-18 15:14:22 +0000

    Co-authored-by: PJMcPrettypants <PJMcPrettypants@users.noreply.github.com>

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -16,14 +16,14 @@ module.exports = {
     localhost: {
       url: "http://127.0.0.1:7545",
     },
-    // hardhat: {
-    //   // url: "http://127.0.0.1:8545",
-    // },
-    // dai: {
-    //   url: "https://dai.poa.network",
-    //   chainId: 100,
-    //   accounts: [process.env.XDAI_PRIVATE_KEY],
-    // },
+    hardhat: {
+      // url: "http://127.0.0.1:8545",
+    },
+    dai: {
+      url: "https://dai.poa.network",
+      chainId: 100,
+      accounts: [process.env.XDAI_PRIVATE_KEY],
+    },
   },
   solidity: "0.8.1",
 };
