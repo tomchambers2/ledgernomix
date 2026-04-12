fb98de90a00730e5b215d5e853a2970c1916ccca
Joe Shellard <joeshellard@gmail.com>
2026-04-12 18:27:27 +0100

    config now points to updated contract

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  gnosis: { gameFactoryContract: { address: "0x3854Bb4595cE9440B8825c9dAD2A9BDB430933C0" } },
+  gnosis: { gameFactoryContract: { address: "0x745621dF33849F061ae2209b55a761097868E974" } },
   local:  { gameFactoryContract: { address: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9" } },
 };
